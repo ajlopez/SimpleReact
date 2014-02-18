eea8a95bc1ec2a24874d2f59ca98a99ae8e1b19a
ajlopez <webmaster@ajlopez.com>
2014-02-18 07:25:42 -0300

    Real implementation of register

--- test/register.js
+++ test/register.js
@@ -7,12 +7,38 @@ exports['Register get and run once using set'] = function (test) {
     var ds = sr.createDataSource();
     var total = 0;
     
-    sr.register(function () {
-        total += ds.get('value');
+    sr.register(function () {        
+        total += ds.get('value');        
+        
+        if (total == 0)
+            return;
+            
         test.ok(total);
         test.equal(total, 10);
         test.done();
     });
     
     ds.set('value', 10);
+};
+
+exports['Register get and run set many times'] = function (test) {
+    test.async();
+    
+    var ds = sr.createDataSource();
+    var total = 0;
+    
+    sr.register(function () {        
+        total += ds.get('value');      
+        
+        if (total < 20)
+            return;
+            
+        test.ok(total);
+        test.equal(total, 20);
+        test.done();
+    });
+
+    setTimeout(function () { ds.set('value', 10); }, 200);
+    setTimeout(function () { ds.set('value', 6); }, 300);
+    setTimeout(function () { ds.set('value', 4); }, 400);
 };
